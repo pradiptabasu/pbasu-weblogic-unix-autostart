3146f72b6f193ae613a493df8e8034196f24f357
Pradipta Basu <pradiptabasu@users.noreply.github.com>
2018-12-20 12:20:25 +0530

    OSM single server management scripts

---
+++ Weblogic-Unix-Startup-Scripts-OSM/osm73514_domain_startWeblogic_Admin.py
@@ -0,0 +1,4 @@
+nmConnect('weblogic','welcome1','ip-172-31-127-93.ec2.internal','5556','osm73514_domain','/home/ec2-user/Oracle/Middleware/Oracle_Home/user_projects/domains/osm73514_domain','plain');
+nmStart('AdminServer');
+nmServerStatus('AdminServer');
+nmDisconnect()
